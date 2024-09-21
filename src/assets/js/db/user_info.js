ac7bfc19f150b8b587699b884cc73b689bcfda2e
tuanphv <tuanpv2604@gmail.com>
2024-09-21 15:09:45 +0700

    demo admin page

---
+++ src/assets/js/db/user_info.js
@@ -0,0 +1,225 @@
+// @ts-nocheck
+var records = [
+    {
+        id: '54654',
+        name: 'Austin Jerde',
+        email: 'li.fritsch@schuppe.com',
+        passwd: 'NhNuCZ5GNY9Hbny',
+        phone_num: '1-453-517-4318 x07027',
+    },
+    {
+        id: '54659',
+        name: 'Priscilla Funk',
+        email: 'arron@wisokygerhold.ca',
+        passwd: 'YzvGNr8HeboPpbe2',
+        phone_num: '(258)570-4441',
+    },
+    {
+        id: '54664',
+        name: 'Kittie Doyle',
+        email: 'theodore@thompson.co.uk',
+        passwd: '3RoCp0ACesU3LW',
+        phone_num: '957.915.1107 x2742',
+    },
+    {
+        id: '54669',
+        name: 'Frankie Ernser',
+        email: 'miriam@haleyvonname',
+        passwd: 'rh_Bbkmcpo2q',
+        phone_num: '783-648-2076',
+    },
+    {
+        id: '54674',
+        name: 'Roseann Murazik',
+        email: 'gina_sanford@roberts.us',
+        passwd: 'M_s9y6X4v1R3izY9',
+        phone_num: '(987)684-9916 x3751',
+    },
+    {
+        id: '54679',
+        name: 'Sherell Stamm',
+        email: 'fransisca.schaden@kozey.us',
+        passwd: 'EpXeCUNyv9',
+        phone_num: '1-306-635-2489',
+    },
+    {
+        id: '54684',
+        name: 'Kelvin Zemlak',
+        email: 'roseanna_koss@fayyundt.com',
+        passwd: 'ggXMajC6fjBJOSUy',
+        phone_num: '1-936-755-9067 x581',
+    },
+    {
+        id: '54689',
+        name: 'Oma Gerlach',
+        email: 'alethea@mcclure.info',
+        passwd: 'mVpSJhk1dQ',
+        phone_num: '1-267-975-8093 x377',
+    },
+    {
+        id: '54694',
+        name: 'Gertie Abshire',
+        email: 'sean@skileskemmer.biz',
+        passwd: 'fWztc6BE',
+        phone_num: '(253)287-5938 x8712',
+    },
+    {
+        id: '54699',
+        name: 'Clarita Bashirian',
+        email: 'latoyia_ebert@gleason.biz',
+        passwd: 'qOhntsfsXX3oAaCW',
+        phone_num: '984-648-0683 x5318',
+    },
+    {
+        id: '54704',
+        name: 'Julia Schumm',
+        email: 'maria.grant@hagenes.co.uk',
+        passwd: 'gHtYHxbrTi1Nk67m',
+        phone_num: '287-273-5032',
+    },
+    {
+        id: '54709',
+        name: 'Taren Stoltenberg',
+        email: 'era@christiansenconn.info',
+        passwd: 'HG8J5Dcr',
+        phone_num: '464.297.9957',
+    },
+    {
+        id: '54714',
+        name: 'Tilda Bernhard',
+        email: 'ciara@gleichnerbeahan.co.uk',
+        passwd: '43cVUnC6fJPrxu2y',
+        phone_num: '447.853.6611',
+    },
+    {
+        id: '54719',
+        name: 'Zachary Hand',
+        email: 'epifania_senger@quigley.ca',
+        passwd: 'js6suExHovLcQSV',
+        phone_num: '1-989-624-2512 x5541',
+    },
+    {
+        id: '54724',
+        name: 'Vella Kirlin',
+        email: 'vita@wolf.co.uk',
+        passwd: 'XfKUV_RhYW',
+        phone_num: '737-901-4656 x82503',
+    },
+    {
+        id: '54729',
+        name: 'Jene Weber',
+        email: 'kitty@hilpert.com',
+        passwd: 'tWdWQVOfSQ7nUzpk',
+        phone_num: '251-694-4800',
+    },
+    {
+        id: '54734',
+        name: 'Shawanda Bogan',
+        email: 'clemmie@stiedemann.ca',
+        passwd: 'KzTdv3RvYYpY',
+        phone_num: '1-339-958-5156 x396',
+    },
+    {
+        id: '54739',
+        name: 'Floy Cronin',
+        email: 'andrew@block.info',
+        passwd: 'dQt9tmpkzeD',
+        phone_num: '272.402.3431 x2898',
+    },
+    {
+        id: '54744',
+        name: 'Zula Mueller',
+        email: 'roman@treutelname',
+        passwd: 'U_qSAuLV54EhtD',
+        phone_num: '544-968-7965 x616',
+    },
+    {
+        id: '54749',
+        name: 'Judith Lesch',
+        email: 'vincenzo@lednername',
+        passwd: 'iSDUhqj9',
+        phone_num: '732.739.1282 x2621',
+    },
+    {
+        id: '1123',
+        name: 'Leanne Graham',
+        email: 'Sincere@april.biz',
+        phone_num: '1-770-736-8031 x56442',
+    },
+    {
+        id: '2853',
+        name: 'Ervin Howell',
+        email: 'Shanna@melissa.tv',
+        phone_num: '010-692-6593 x09125',
+    },
+    {
+        id: '3123',
+        name: 'Clementine Bauch',
+        email: 'Nathan@yesenia.net',
+        phone_num: '1-463-123-4447',
+    },
+    {
+        id: '4121',
+        name: 'Patricia Lebsack',
+        email: 'Julianne.OConner@kory.org',
+        phone_num: '493-170-9623 x156',
+    },
+    {
+        id: '5675',
+        name: 'Chelsey Dietrich',
+        email: 'Lucio_Hettinger@annie.ca',
+        phone_num: '(254)954-1289',
+    },
+    {
+        id: '6321',
+        name: 'Mrs. Dennis Schulist',
+        email: 'Karley_Dach@jasper.info',
+        phone_num: '1-477-935-8478 x6430',
+    },
+    {
+        id: '7223',
+        name: 'Kurtis Weissnat',
+        email: 'Telly.Hoeger@billy.biz',
+        phone_num: '210.067.6132',
+    },
+    {
+        id: '8321',
+        name: 'Nicholas Runolfsdottir V',
+        email: 'Sherwood@rosamond.me',
+        phone_num: '586.493.6943 x140',
+    },
+    {
+        id: '9113',
+        name: 'Glenna Reichert',
+        email: 'Chaim_McDermott@dana.io',
+        phone_num: '(775)976-6794 x41206',
+    },
+    {
+        id: '1023',
+        name: 'Clementina DuBuque',
+        email: 'Rey.Padberg@karina.biz',
+        phone_num: '024-648-3804',
+    },
+];
+renderUser(records);
+function renderUser(list) {
+    let element = `<tr>
+        <th>Check<i class="fa-solid fa-filter"></i></th>
+        <th>ID<i class="fa-solid fa-filter"></i></th>
+        <th>Name<i class="fa-solid fa-filter"></i></th>
+        <th>Email<i class="fa-solid fa-filter"></i></th>
+        <th>Phone number<i class="fa-solid fa-filter"></i></th>
+        <th>Rule<i class="fa-solid fa-filter"></i></th>
+    </tr>`;
+    list.forEach((user, index) => {
+        element += `<tr id="row-${index % 2}">
+            <td><input type="checkbox" style="margin-left: 20px"></td>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.phone_num}</td>
+            <td></td>
+        </tr>`;
+    });
+    document.getElementById('user_table').innerHTML = element;
+}
