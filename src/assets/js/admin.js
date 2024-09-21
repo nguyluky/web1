1c8f5a9996be4ca8598a53c8ad6c19b743f089db
tuanphv <tuanpv2604@gmail.com>
2024-09-21 18:51:11 +0700

    add search feature

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -1,4 +1,7 @@
 import fackDatabase from './db/fakeDb.js';
 import renderUser from './render/user_info_table.js';
+import searchUser from './search.js';
 
 renderUser(fackDatabase.getAllUserInfo());
+let input = document.getElementById('search-input');
+if (input) input.oninput = searchUser;
