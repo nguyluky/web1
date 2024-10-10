3eb4bc23b30b95994bebdffa545781ac5d7176cd
luky <151525707+nguyluky@users.noreply.github.com>
2024-10-10 12:00:18 +0700

    [ADD] css

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,6 +2,9 @@ console.log('Popup function success');
 const btnLocation = document.getElementById('btn-location');
 const closePopup = document.getElementById('btn-close');
 const popup_wrapper = document.getElementById('popup-wrapper');
+
+const btnAccount = document.getElementById('btn-acount');
+
 if (btnLocation && closePopup && popup_wrapper) {
     btnLocation.addEventListener('click', () => {
         popup_wrapper.classList.add('show');
@@ -9,7 +12,9 @@ if (btnLocation && closePopup && popup_wrapper) {
 
     // NOTE: nếu mà nhấn mà nó nó chứa thằng popup thì là nhấn bên ngoài
     popup_wrapper.onclick = (event) => {
-        const popup = /**@type {HTMLElement}*/ (event.target).querySelector('.popup');
+        const popup = /**@type {HTMLElement}*/ (event.target).querySelector(
+            '.popup',
+        );
         if (popup) popup_wrapper.classList.remove('show');
     };
 
