c45022edc9e5f84a379fc479512c768c956681ff
tuanphv <tuanpv2604@gmail.com>
2024-11-16 09:54:53 +0700

    biếng

--- src/assets/js/render/table/orderTabel.js
+++ src/assets/js/render/table/orderTabel.js
@@ -39,6 +39,12 @@ let cacheEdit = {};
 function handleOnChange(data, key, newValue) {
     console.log('onchange called');
 
+    if (!cacheEdit[data.id]) {
+        cacheEdit[data.id] = {
+            ...data,
+            [key]: newValue,
+        };
+    }
     cacheEdit[data.id] = {
         ...cacheEdit[data.id],
         [key]: newValue,
@@ -372,7 +378,7 @@ function createDropdownFilter(options) {
         const option = options.find((e) => e.title == select.value);
 
         if (!option) return;
-        option.onOk(/** @type {HTMLElement | null} */ (body.firstChild));
+        option.onOk(/** @type {HTMLElement | null} */(body.firstChild));
 
         // TODO: note
         document.body.click();
@@ -382,7 +388,7 @@ function createDropdownFilter(options) {
         const option = options.find((e) => e.title == select.value);
 
         if (!option) return;
-        option.onCancel(/** @type {HTMLElement | null} */ (body.firstChild));
+        option.onCancel(/** @type {HTMLElement | null} */(body.firstChild));
 
         // TODO: note
         document.body.click();
