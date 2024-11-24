128548c894afa489f38d0120f14f61378aa5eadc
JustAnother-12 <bathiennguyen024@gmail.com>
2024-11-24 13:13:46 +0700

    add event cho nút mua ngay, thêm responsive cho thanh toán

--- src/assets/js/pages/product/index.js
+++ src/assets/js/pages/product/index.js
@@ -2,7 +2,8 @@ import fakeDatabase from '../../db/fakeDBv1.js';
 import { toast } from '../../render/popupRender.js';
 import { formatNumber, text2htmlElement } from '../../until/format.js';
 import { addStyle, errorPage, navigateToPage, removeStyle } from '../../until/router.js';
-import { pushCartItemIntoCart, updateCartQuantity } from '../cart/cart.js';
+import uuidv from '../../until/uuid.js';
+import { buyBooks, pushCartItemIntoCart, updateCartQuantity } from '../cart/cart.js';
 
 let SPtt = [];
 let Current_Page = 1;
@@ -304,6 +305,22 @@ function setEventListener(product_id) {
         category.addEventListener('click', e => {
             navigateToPage('home', { c: String(category.dataset.category) });
         });
+    });
+
+    const buy_now = document.querySelector('.buy-now');
+    buy_now?.addEventListener('click', async e => {
+        const user_id = localStorage.getItem('user_id');
+        if (!user_id) {
+            toast({
+                message: 'Vui lòng đăng nhập để mua hàng',
+                type: 'error'
+            })
+            return;
+        }
+        const cart_id = uuidv(10);
+        await fakeDatabase.createCartItem(cart_id, user_id, product_id, Number(inputquantity.value));
+        updateCartQuantity();
+        navigateToPage('payment', { payment: cart_id });
     });
 
 }
